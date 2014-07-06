712e6a730a79473582585c05391aa31f8a298d70
Emil Nilsson <emil_nilsson_96@live.se>
2014-07-07 00:44:45 +0200

    Create en.ini.php

---
+++ en.ini.php
@@ -0,0 +1,11 @@
+<?php return; ?>;
+
+; #MESSAGES
+message[register_success]		= "Your account has been created!";
+message[register_fail]			= "Something went wrong, try again!";
+message[user_exists]			= "Username already exists";
+message[email_exists] 			= "Email already exists";
+message[passwords_not_equal] 	= "Your passwords doesn't match each other";
+message[welcome]				= "Welcome dear user!";
+
+; This is a comment
